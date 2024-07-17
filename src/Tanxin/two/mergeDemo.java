6905938f1d98fae05f2900a3e1312e51caf36f5b
jiangyanming <1635156042@qq.com>
2024-07-17 21:01:47 +0800

    add

--- src/Tanxin/two/mergeDemo.java
+++ src/Tanxin/two/mergeDemo.java
@@ -9,23 +9,27 @@ public class mergeDemo {
 
     public int[][] merge(int[][] intervals) {
 
-        List<List> result = new ArrayList<>();
-        List path = new ArrayList();
+        List<int[]> result = new ArrayList<>();
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[0] - o2[0];
             }
         });
-
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] <= intervals[i - 1][1]) {
-                intervals[i][0]=Math.min(intervals[i][0],intervals[i-1][0]);
-                intervals[i][1] = Math.max(intervals[i][1], intervals[i - 1][1]);
-                path.add(intervals[i][0]);
-                path.add(intervals[i][1]);
+            if (intervals[i][0] > end) {
+                result.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else {
+                //更新最新最右的边界
+                end = Math.max(end, intervals[i][1]);
             }
         }
-
+        //加上最后的结果：
+        result.add(new int[]{start, end});
+        return result.toArray(new int[result.size()][]);
     }
 }
